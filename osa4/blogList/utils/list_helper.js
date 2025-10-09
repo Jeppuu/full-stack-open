14cc2363d589351204ed46bbc894dec82a8eb91c
Jeppuu <blomqvist.jennam@gmail.com>
2025-10-09 14:09:24 +0300

    yksikkotesteja step3

--- osa4/blogList/utils/list_helper.js
+++ osa4/blogList/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  return blogs.reduce(
+    (fav, blog) => (blog.likes > fav.likes ? blog : fav),
+    blogs[0]
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
