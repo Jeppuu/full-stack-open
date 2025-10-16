7a9235e065d6d0a3a3c175f2f18db81a51260fcd
Jeppuu <blomqvist.jennam@gmail.com>
2025-10-16 17:05:35 +0300

    add user creation

--- osa4/blogList/models/blog.js
+++ osa4/blogList/models/blog.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const User = require("./user");
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 blogSchema.set("toJSON", {
---
+++ osa4/blogList/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  name: String,
+  passwordHash: { type: String, required: true },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+UserSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
